994400c7a2bcad905e34e6c31ffc07bd195dc887
Alexandru Oprean <alexandru@oprean.net>
2021-01-11 20:56:45 +0200

    Adding EventManager

---
+++ EventManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EventName { };
+
+public class EventManager : MonoBehaviour
+{
+    private Dictionary<EventName, Action<dynamic>> events;
+
+    private static EventManager _instance;
+    public static EventManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        events = new Dictionary<EventName, Action<dynamic>>();
+    }
+
+    public static void StartListening(EventName eventEnum, Action<dynamic> listener)
+    {
+        Action<dynamic> currentEvent;
+        if (Instance.events.TryGetValue(eventEnum, out currentEvent))
+        {
+            currentEvent += listener;
+            Instance.events[eventEnum] = currentEvent;
+        }
+        else
+        {
+            currentEvent += listener;
+            Instance.events.Add(eventEnum, currentEvent);
+        }
+    }
+
+    public static void StopListening(EventName eventEnum, Action<dynamic> listener)
+    {
+        Action<dynamic> currentEvent;
+        if (Instance.events.TryGetValue(eventEnum, out currentEvent)&& currentEvent != null)
+        {
+            currentEvent -= listener;
+            Instance.events[eventEnum] = currentEvent;
+        }
+    }
+
+    public static void TriggerEvent(EventName eventEnum, dynamic data)
+    {
+        Action<dynamic> currentEvent = null;
+        if (Instance.events.TryGetValue(eventEnum, out currentEvent) && currentEvent != null)
+        {
+            currentEvent.Invoke(data);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # EventManager
+
+This Event Manager is made special for small-medium Unity games where simplicity is preferred over performance.
+
+Usage:
+1. Copy the file in your Unity project;
+2. Add the EventManager.cs to a GameObject in your Scene (GameManager or EventManager preferred);
+3. Declare your events in EventName enum;
+4. You can Subscribe and Unsubscribe really easy by using StartListening and StopListening methods;
+5. Use TriggerEvent method to trigger the event you wish for;
+
+Advantages:
+- Easy to add in a project;
+- Easy to use;
+- Using dynamic keyword you can use primitive types or complex objects really easy;
+
+Disadvantages:
+- It has a lower performance:
+  - For 1000 events triggered sincroniously:
+    - **dynamic**: aprox. 350 miliseconds;
+    - **string**: aprox. 130 miliseconds;
+    - **int**: aprox, 130 miliseconds;
+    - **object**: aprox. 150 miliseconds;
+- Not having the parameteres stronged typed the errors in using wrong the models can be seen only at runtime;
